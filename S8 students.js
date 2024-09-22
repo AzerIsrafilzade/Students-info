84b5f028a698cc921a61ed3a0ac396f925e836b5
AzerIsrafilzade <azisrafilzade@gmail.com>
2024-09-22 17:43:27 +0400

    S8 students completed

---
+++ S8 students.js
@@ -0,0 +1,77 @@
+telebeler = [
+
+{ 
+    ad:"Elmar",
+    soyad:"Misirzade",
+    projectTask:80,
+    ispassed:true
+},
+
+{ 
+    ad:"Elgun",
+    soyad:"Resulov",
+    projectTask:85,
+    ispassed:true
+},
+
+{ 
+    ad:"Emine",
+    soyad:"Abayeva",
+    projectTask:80,
+    ispassed:true
+},
+
+{ 
+    ad:"Ayshen",
+    soyad:"Həsənova",
+    projectTask:75,
+    ispassed:true
+},
+
+{ 
+    ad:"Fidan ",
+    soyad:"Sultanlı",
+    projectTask:75,
+    ispassed:true
+},
+
+{ 
+    ad:"Seva",
+    soyad:"Quseynova",
+    projectTask:70,
+    ispassed:true
+},
+
+{ 
+    ad:"Azer",
+    soyad:"Israfilzade",
+    projectTask:80,
+    ispassed:true
+},
+
+{ 
+    ad:"elnur",
+    soyad:"rustamov",
+    projectTask:70,
+    ispassed:true
+},
+
+{ 
+    ad:"Səadət",
+    soyad:"Məmmədova",
+    projectTask:70,
+    ispassed:true
+},
+
+{ 
+    ad:"Rehim",
+    soyad:"Qedirov",
+    projectTask:50,
+    ispassed:null
+},
+
+]
+
+for( var i=0; i<telebeler.length; i++) {
+    console.log(telebeler[i])
+}
